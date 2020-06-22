67bb7b7fb1625cfb16cdeca91526c79a2d591e81
Blackstone <coolerdude57@gmail.com>
2020-06-22 15:56:41 -0400

    Initial commit

---
+++ BsWebServer/HostAddress.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BsWebServer.Addresses
+{
+    public class HostAddress
+    {
+        public string IpAddress;
+        public string Port;
+
+        public HostAddress(string IpAddress, string Port)
+        {
+            this.IpAddress = IpAddress;
+            this.Port = Port;
+        }
+
+        public HostAddress(string IpAddress)
+        {
+            this.IpAddress = IpAddress;
+            this.Port = "";
+        }
+
+        public static HostAddress Parse(string stAddress)
+        {
+            string[] temp = stAddress.Split(':');
+            if (temp.Length > 1)
+            {
+                return new HostAddress(temp[0], temp[1]);
+            }
+            else
+            {
+                return new HostAddress(temp[0]);
+            }
+        }
+
+        public override string ToString()
+        {
+            if (Port == String.Empty)
+            {
+                return IpAddress;
+            }
+            else
+            {
+                return IpAddress + ":" + Port;
+            }
+        }
+
+        public IPAddress ToIPAddress()
+        {
+            return IPAddress.Parse(IpAddress);
+        }
+    }
+}
---
+++ BsWebServer/HttpListener.cs
@@ -0,0 +1,72 @@
+﻿using BsWebServer.Events;
+using BsWebServer.Handling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BsWebServer.Listeners
+{
+    public class HttpListener
+    {
+        public event EventHandler<ServerEventArgs> WebServerRequest;
+
+        string ip;
+        int port;
+        public HttpListener(string ip, int port)
+        {
+            this.ip = ip;
+            this.port = port;
+        }
+
+        public void Listen()
+        {
+            TcpListener server = null;
+
+            server = new TcpListener(IPAddress.Parse(ip), port);
+            server.Start();
+
+            Byte[] bytes = new Byte[512];
+            String data = null;
+
+            try
+            {
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+
+                    data = null;
+
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+                    i = stream.Read(bytes, 0, bytes.Length);
+
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+
+                    HttpRequest httpRequest = new HttpRequest(data);
+
+                    HttpResponse httpResponse = new HttpResponse(stream, client);
+
+                    OnWebServerRequest(new ServerEventArgs(httpResponse, httpRequest));
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+
+        protected virtual void OnWebServerRequest(ServerEventArgs se)
+        {
+            WebServerRequest?.Invoke(this, se);
+        }
+    }
+}
